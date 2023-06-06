f7f490077d465729e111635f8be007562767f665
placidojr12 <61884076+placidojr12@users.noreply.github.com>
2023-06-06 01:45:10 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,44 +1,77 @@
-import math
+import cmath
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 
 def calcular_bhaskara(a, b, c):
     delta = b**2 - 4*a*c
 
-    if delta < 0:
-        messagebox.showinfo("Resultado", "A equação não possui raízes reais.")
+    if delta >= 0:
+        x1 = (-b + cmath.sqrt(delta)) / (2*a)
+        x2 = (-b - cmath.sqrt(delta)) / (2*a)
+        resultado = f"As raízes da equação são:\nx1 = {x1:.2f}\nx2 = {x2:.2f}"
     else:
-        x1 = (-b + math.sqrt(delta)) / (2*a)
-        x2 = (-b - math.sqrt(delta)) / (2*a)
-        messagebox.showinfo("Resultado", f"As raízes da equação são:\nx1 = {x1}\nx2 = {x2}")
+        x1 = (-b + cmath.sqrt(delta)) / (2*a)
+        x2 = (-b - cmath.sqrt(delta)) / (2*a)
+        resultado = f"As raízes da equação são:\nx1 = {x1:.2f} + {x1.imag:.2f}j\nx2 = {x2:.2f} + {x2.imag:.2f}j\nAs raízes não são reais."
 
-def calcular():
-    coeficiente_a = float(entry_a.get())
-    coeficiente_b = float(entry_b.get())
-    coeficiente_c = float(entry_c.get())
+    dialogo_resultado(resultado)
 
-    calcular_bhaskara(coeficiente_a, coeficiente_b, coeficiente_c)
+def dialogo_resultado(resultado):
+    dialogo = tk.Toplevel()
+    dialogo.title("Resultado")
+    dialogo.geometry("300x120")
+    dialogo.resizable(False, False)
+    dialogo.configure(bg="#f0f0f0")
+
+    frame = ttk.Frame(dialogo, padding="20")
+    frame.pack(fill="both", expand=True)
+
+    label = ttk.Label(frame, text=resultado)
+    label.pack(pady=5)
+
+    btn_ok = ttk.Button(frame, text="OK", command=dialogo.destroy)
+    btn_ok.pack(pady=10)
+
+def calcular(event=None):
+    try:
+        coeficiente_a = float(entry_a.get())
+        coeficiente_b = float(entry_b.get())
+        coeficiente_c = float(entry_c.get())
+
+        calcular_bhaskara(coeficiente_a, coeficiente_b, coeficiente_c)
+    except ValueError:
+        messagebox.showerror("Erro", "Por favor, insira valores numéricos válidos para os coeficientes.")
 
 # Criação da interface gráfica
 window = tk.Tk()
 window.title("Calculadora de Bhaskara")
+window.geometry("300x280")
+window.resizable(False, False)
+window.configure(bg="#f0f0f0")
 
-label_a = tk.Label(window, text="Coeficiente a:")
-label_a.pack()
-entry_a = tk.Entry(window)
-entry_a.pack()
+main_frame = ttk.Frame(window, padding="20")
+main_frame.pack(fill="both", expand=True)
 
-label_b = tk.Label(window, text="Coeficiente b:")
-label_b.pack()
-entry_b = tk.Entry(window)
-entry_b.pack()
+label_a = ttk.Label(main_frame, text="Coeficiente a:")
+label_a.pack(pady=5)
+entry_a = ttk.Entry(main_frame)
+entry_a.pack(pady=5)
+entry_a.bind("<Return>", calcular)
 
-label_c = tk.Label(window, text="Coeficiente c:")
-label_c.pack()
-entry_c = tk.Entry(window)
-entry_c.pack()
+label_b = ttk.Label(main_frame, text="Coeficiente b:")
+label_b.pack(pady=5)
+entry_b = ttk.Entry(main_frame)
+entry_b.pack(pady=5)
+entry_b.bind("<Return>", calcular)
 
-btn_calcular = tk.Button(window, text="Calcular", command=calcular)
-btn_calcular.pack()
+label_c = ttk.Label(main_frame, text="Coeficiente c:")
+label_c.pack(pady=5)
+entry_c = ttk.Entry(main_frame)
+entry_c.pack(pady=5)
+entry_c.bind("<Return>", calcular)
+
+btn_calcular = ttk.Button(main_frame, text="Calcular", command=calcular)
+btn_calcular.pack(pady=10)
 
 window.mainloop()
